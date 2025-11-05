f949d5467d611e6f56e7657d94cf61bcaa582cd3
visob <victoriasobral7@gmail.com>
2025-11-04 22:41:33 -0300

    Animación: TRANSICION ENTRE PÁGINAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Outlet, useLocation } from "react-router-dom";
 //  Importar componentes de layout
 import Sidebar from "./components/sidebar/Sidebar";
 import Footer from "./components/footer/Footer.jsx";
-
 // Importar los componentes de las páginas
 import HomePage from "./pages/Home";
 import BitacoraPage from "./pages/Bitacora";
@@ -12,9 +10,21 @@ import IntegrantePage from "./pages/IntegrantePage";
 import Galeria from "./pages/GaleriaJsonPage";
 import ApiPage from "./pages/Apipage";
 import Diagrama from "./pages/Diagrama.jsx";
+// Importar CSS de transiciones
+import "./css/transiciones.css";
 
-// --- Componente Layout ---
+// --- Componente Layout con Animaciones ---
 const Layout = () => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransitionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) {
+      setTransitionStage("fadeOut");
+    }
+  }, [location, displayLocation]);
+
   return (
     <div style={{ display: "flex" }}>
       <Sidebar />
@@ -28,8 +38,24 @@ const Layout = () => {
         }}
       >
         <main style={{ flex: "1 0 auto" }}>
-          {/* Outlet es el marcador de posición donde React Router renderizará la página actual */}
-          <Outlet />
+          <div
+            className={`page-transition ${transitionStage}`}
+            onAnimationEnd={() => {
+              if (transitionStage === "fadeOut") {
+                setTransitionStage("fadeIn");
+                setDisplayLocation(location);
+              }
+            }}
+          >
+            <Routes location={displayLocation}>
+              <Route index element={<HomePage />} />
+              <Route path="bitacora" element={<BitacoraPage />} />
+              <Route path="galeria" element={<Galeria />} />
+              <Route path="apipage" element={<ApiPage />} />
+              <Route path="diagrama" element={<Diagrama />} />
+              <Route path="integrantes/:id" element={<IntegrantePage />} />
+            </Routes>
+          </div>
         </main>
         <Footer />
       </div>
@@ -41,16 +67,7 @@ const Layout = () => {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="bitacora" element={<BitacoraPage />} />
-        <Route path="galeria" element={<Galeria />} />
-        <Route path="apipage" element={<ApiPage />} />
-        <Route path="diagrama" element={<Diagrama />} />
-
-        {/* La ruta vuelve a ser como al principio */}
-        <Route path="integrantes/:id" element={<IntegrantePage />} />
-      </Route>
+      <Route path="/*" element={<Layout />} />
     </Routes>
   );
 }
